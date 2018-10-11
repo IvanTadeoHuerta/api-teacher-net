5ef663f232950055d111ca036f38f487ef05e7bc
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-10-11 16:12:11 -0500

    CRUD PERFILES

--- controllers/perfil.js
+++ controllers/perfil.js
@@ -16,17 +16,31 @@ function registroPerfil(req, res) {
 
     if (params.nombre && params.descripcion) {
         if ("" + params.nombre.trim() && "" + params.nombre.trim()) {
-            perfil.save((err, perfilStored) => {
+
+            Perfil.find({ 'nombre': params.nombre }, function (err, records) {
+
                 if (err) {
                     res.status(500).send({ message: 'Error al guardar perfil' });
                 } else {
-                    if (!perfilStored) {
-                        res.status(500).send({ message: 'No se ha registrado el perfil' });
+                    if (records.length == 0) {
+                        perfil.save((err, perfilStored) => {
+                            if (err) {
+                                res.status(500).send({ message: 'Error al guardar perfil' });
+                            } else {
+                                if (!perfilStored) {
+                                    res.status(500).send({ message: 'No se ha registrado el perfil' });
+                                } else {
+                                    res.status(200).send({ message: 'Exito', perfil: perfilStored });
+                                }
+                            }
+                        });
                     } else {
-                        res.status(200).send({ message: 'Exito', perfil: perfilStored });
+                        res.status(400).send({ message: 'Ya existe un perfil con ese nombre' });
                     }
                 }
             });
+
+
         } else {
             res.status(400).send({ message: 'Los campos no pueden estar vacios' });
         }
@@ -38,6 +52,70 @@ function registroPerfil(req, res) {
 
 }
 
+function updatePerfil(req, res) {
+    var idPerfil = req.params.id;
+    var update = req.body;
+
+
+    if (update.nombre && update.descripcion) {
+
+        Perfil.find({ 'nombre': update.nombre }, function (err, records) {
+            if (err) {
+                res.status(500).send({ message: 'Error al actualizar el perfil' });
+            } else {
+                Perfil.findByIdAndUpdate(idPerfil, update, (err, perfilUpdated) => {
+                    if (err) {
+                        res.status(500).send({ message: 'Error al actualizar el perfil' });
+                    } else {
+                        if (!perfilUpdated) {
+                            res.status(404).send({ message: 'No se pudo actualizar el perfil' });
+                        } else {
+                            res.status(200).send({ perfil: perfilUpdated });
+                        }
+                    }
+                });
+            }
+        });
+
+    } else {
+        res.status(400).send({ message: 'Por favor verifique los datos capturados' });
+    }
+
+}
+
+function getPerfil(req, res) {
+    var perfilId = req.params.id;
+    Perfil.findById(perfilId, (err, perfil) => {
+        if (err) {
+            res.status(500).send({ message: 'Error al consultar Perfil' });
+        } else {
+            if (!perfil) {
+                res.status(404).send({ message: 'El perfil no existe' });
+            } else {
+                res.status(200).send({ perfil });
+            }
+        }
+    });
+}
+
+function getPerfiles(req, res) {
+    Perfil.find().sort('nombre').exec((err, perfiles) => {
+        if (err) {
+            res.status(500).send({ message: 'Error al consultar perfiles' });
+        } else {
+            if (!perfiles) {
+                res.status(404).send({ message: 'No existen perfiles' });
+            } else {
+                res.status(200).send({ perfiles });
+            }
+        }
+    });
+}
+
+
 module.exports = {
-    registroPerfil
+    registroPerfil,
+    updatePerfil,
+    getPerfil,
+    getPerfiles
 }
--- routes/perfil.js
+++ routes/perfil.js
@@ -6,6 +6,9 @@ var md_auth = require('../auth/authenticated');
 var api = express.Router();
 
 api.post('/registro-perfil', PerfilController.registroPerfil);
+api.put('/update-perfil/:id', PerfilController.updatePerfil);
+api.get('/get-perfil/:id', PerfilController.getPerfil);
+api.get('/get-perfiles', PerfilController.getPerfiles);
 
 
 module.exports = api;
